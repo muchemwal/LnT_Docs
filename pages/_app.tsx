8ffd83e925ee71ef20f7b0a95c55dcf5d5804485
Langton <muchemwal@gmail.com>
2026-07-01 15:26:46 +0200

    style: add image sizing for mobile screenshots
    - Mobile screenshots capped at 320px width with rounded borders
    - .mobile-screens class for side-by-side layout
    - Web screenshots full-width with subtle border
    - Dark mode support for all image borders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/swagger-overrides.css';
+import '../styles/docs.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
